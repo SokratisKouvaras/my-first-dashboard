71d212784e5aaa0613ba06fd4f9f0e7ce1305398
Sokratis Kouvaras <sokrateskouv@gmail.com>
2021-01-17 12:43:00 +0100

    Add mockup dashboard for starts

---
+++ helpers.R
@@ -0,0 +1,20 @@
+# Helper functions -------------------------------------------------------------
+
+create_default_datatable <- function(df, selection = "none", options = NULL, ...){
+  if (is.null(options)){
+    options <- get_default_datatable_options(df)
+  }
+  df %>%
+    datatable(rownames = FALSE, escape = FALSE, selection=selection, options = options, ...)
+}
+
+#' Getter for datatable default options
+#' @param df the target dataframe
+#' @return list of options
+get_default_datatable_options <- function(df){
+  list(dom = "t", 
+       ordering = TRUE, 
+       pageLength = -1, 
+       columnDefs = list(list(className = "dt-center",
+                              targets = 1:(ncol(df)-1))))
+}
