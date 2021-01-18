82cd668135430e72988df1feae1a8569eae6a9a9
Sokratis Kouvaras <sokrateskouv@gmail.com>
2021-01-18 07:45:19 +0100

    Move project initialization from global.R to run.R

--- run.R
+++ run.R
@@ -1,5 +1,23 @@
+# Load all the packages we want!
+library(shinydashboard)
+library(shinydashboardPlus)
 library(shiny)
+library(shinythemes)
+library(shinyBS)
+library(anytime)
+library(DT)
+library(leaflet)
+library(ggplot2)
+library(ggwordcloud)
+library(plotly)
+library(readxl)
+library(dplyr)
 
+# Source additional configuration files and helper functions
+source("config.R")
+source("helpers.R")
+
+covid_dataset <- get_dataset()
 port <- Sys.getenv('PORT')
 
 shiny::runApp(appDir = getwd(),
